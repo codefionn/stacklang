bdcba0383505608ec0793ae3129baeef5df54dee
Fionn Langhans <fionn.langhans@gmail.com>
2019-05-04 16:59:13 +0200

    Main and build file.

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "stacklang/global.h"
+#include "stacklang/stack.h"
+#include "stacklang/lexer.h"
+#include "stacklang/syntax.h"
+
+int main(int argsc, char * argsv[]) {
+
+  return EXIT_SUCCESS;
+}
